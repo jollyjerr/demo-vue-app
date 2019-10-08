d9e0dc441730f54252fafeca7b6464fa2bf7b4e2
jollyjerr <jollyjerr@gmail.com>
2019-10-08 10:29:22 -0600

    add header

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,22 @@
 <template>
   <div id="app">
+    <header>
+      <img src="https://www.dhresource.com/0x0s/f2-albu-g5-M00-8C-01-rBVaI1h0b7uAAw3eAADOgwXBYRU213.jpg/hot-sale-pet-paw-print-with-heart-dog-funny.jpg" alt="logo">
+      <h1>BarkWire</h1>
+    </header>
   </div>
 </template>
 
 <style lang="scss">
+  #app {
+    header {
+      display: flex;
+      justify-content: flex-start;
+      align-items: center;
+      background-color: hsl(240, 30%, 80%);
+      img {
+        width: 150px;
+      }
+    }
+  }
 </style>
