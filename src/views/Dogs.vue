e8406b6a3cbee3717c490db2f1926880649ce29a
jollyjerr <jollyjerr@gmail.com>
2019-10-08 10:44:23 -0600

    dynamic dog cards from state

--- src/views/Dogs.vue
+++ src/views/Dogs.vue
@@ -2,27 +2,34 @@
     <div class="dogs">
         <h2>Dogs</h2>
         <ul>
-            <li>
+            <li v-for="dog in dogs">
                 <div class="dog-listing">
-                    <img src="https://hips.hearstapps.com/wdy.h-cdn.co/assets/17/39/1506709524-cola-0247.jpg?crop=1.00xw:0.750xh;0,0.226xh&resize=480:*" alt="dog">
-                    <h3>Bower</h3>
-                </div>
-            </li>
-            <li>
-                <div class="dog-listing">
-                    <img src="https://hips.hearstapps.com/wdy.h-cdn.co/assets/17/39/1506709524-cola-0247.jpg?crop=1.00xw:0.750xh;0,0.226xh&resize=480:*" alt="dog">
-                    <h3>Bower</h3>
-                </div>
-            </li>
-            <li>
-                <div class="dog-listing">
-                    <img src="https://hips.hearstapps.com/wdy.h-cdn.co/assets/17/39/1506709524-cola-0247.jpg?crop=1.00xw:0.750xh;0,0.226xh&resize=480:*" alt="dog">
-                    <h3>Bower</h3>
+                    <img :src="dog.imageUrl" alt="dog">
+                    <h3>{{dog.name}}</h3>
                 </div>
             </li>
         </ul>
     </div>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            dogs: [{
+                name: "Bower",
+                imageUrl: 'https://hips.hearstapps.com/wdy.h-cdn.co/assets/17/39/1506709524-cola-0247.jpg?crop=1.00xw:0.750xh;0,0.226xh&resize=480:*'
+            }, {
+                name: "Waggy",
+                imageUrl: 'https://hips.hearstapps.com/wdy.h-cdn.co/assets/17/39/1506709524-cola-0247.jpg?crop=1.00xw:0.750xh;0,0.226xh&resize=480:*'
+            }, {
+                name: "Percy",
+                imageUrl: 'https://hips.hearstapps.com/wdy.h-cdn.co/assets/17/39/1506709524-cola-0247.jpg?crop=1.00xw:0.750xh;0,0.226xh&resize=480:*'
+            }]
+        }
+    }
+}
+</script>
 
 <style lang="scss">
     .dogs {
